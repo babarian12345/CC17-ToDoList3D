036a04204d8f8e51ce5a2b42a00b47da5c390277
Palaruan Wyndel <44886677+babarian12345@users.noreply.github.com>
2021-11-13 21:18:12 +0800

    intent

---
+++ app/src/main/java/com/example/cc17_3d_toolist/AddActivity.kt
@@ -0,0 +1,13 @@
+package com.example.cc17_3d_toolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class AddActivity : AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+
+    }
+}
--- app/src/main/java/com/example/cc17_3d_toolist/MainActivity.kt
+++ app/src/main/java/com/example/cc17_3d_toolist/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.cc17_3d_toolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_add = findViewById<TextView>(R.id.btn_add)
+
+        btn_add.setOnClickListener {
+            intent = Intent(this,AddActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
